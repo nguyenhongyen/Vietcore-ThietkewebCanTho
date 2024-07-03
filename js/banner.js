3b7b6a932bb0281bd2338e64a286b19a5edbf690
Vietcore-Yen <hongyen.27nguyen@gmail.com>
2024-07-03 10:18:32 +0700

    update3

--- js/banner.js
+++ js/banner.js
@@ -12,13 +12,19 @@
 				$('body').removeClass('show-nav show-overlay');
 			})
 		}
+		$(document).on('click', function (event) {
+			if (!event.target.closest('.header-nav') && !event.target.closest('#callNav')) {
+				$('body').removeClass('show-nav show-overlay');
+			}
+		});
 
 	}
 	const handleInitHeader = function (){
 		let headerPosition = $('header').offset().top;
 		$(window).scroll(function () {
 			var scrollValue = $(window).scrollTop();
-			if (scrollValue > headerPosition) {
+
+			if (scrollValue > 600) {
 				$('header').addClass('is-scroll');
 			} else {
 				$('header').removeClass('is-scroll');
